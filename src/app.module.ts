5fb80e36d1241aa1cc02000ea9c7d83480ff172d
wsk0715 <leindey@gmail.com>
2025-05-21 23:29:11 +0900

    HTTP Request 로거 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { LoggerMiddleware } from './common/middleware/logger.middleware';
 
 @Module({
   imports: [
@@ -16,4 +17,10 @@ import { UserModule } from './user/user.module';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes({ path: '*', method: RequestMethod.ALL });
+  }
+}
---
+++ src/common/middleware/logger.middleware.ts
@@ -0,0 +1,14 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    console.log('--- [HTTP Request] ---');
+    console.log(`${req.method} ${req.originalUrl} HTTP/${req.httpVersion}`);
+    console.log('Headers:', req.headers);
+    console.log('Body:', req.body);
+    console.log('------------------------');
+    next();
+  }
+}
