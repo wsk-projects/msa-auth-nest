7437e9e8e030cf4bba520bcfa207072094f64b62
wsk0715 <leindey@gmail.com>
2025-05-22 10:51:32 +0900

    ESLint 설정 수정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -29,6 +29,7 @@ export default tseslint.config(
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
+      'prettier/prettier': ['error', { printWidth: 120 }],
       'linebreak-style': 'off',
     },
   },
