25314930ae529d63ce31ecd562d9ff6ed7e961db
wsk0715 <leindey@gmail.com>
2025-05-22 11:31:39 +0900

    ESLint 설정 수정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -26,11 +26,12 @@ export default tseslint.config(
   },
   {
     rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn',
-      'prettier/prettier': ['error', { printWidth: 120 }],
-      'linebreak-style': 'off',
+      '@typescript-eslint/no-explicit-any': 'off', // any 타입 사용 허용
+      '@typescript-eslint/no-floating-promises': 'warn', // 처리되지 않은 Promise 경고
+      '@typescript-eslint/no-unsafe-argument': 'warn', // 안전하지 않은 파라미터 사용 경고
+      '@typescript-eslint/no-empty-object-type': 'off', // 빈 객체 타입 허용
+      'prettier/prettier': ['error', { printWidth: 120 }], // 한 줄 최대 길이 120자
+      'linebreak-style': 'off', // 줄바꿈 스타일 검사 비활성화(CRLF & LF)
     },
   },
 );
